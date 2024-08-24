55aa704dc2ced5837397e4d43c43f314aa574cad
lilianaperezdiaz <lilianaperezdiaz2525@gmail.com>
2024-08-24 14:47:59 -0400

    SQL Challenge

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -1,1 +1,51 @@
-CREATE TABLE 
+CREATE TABLE titles(
+	title_id VARCHAR(40),
+	title varchar(20),
+	PRIMARY KEY (title_id) 
+);
+
+CREATE TABLE employees(
+	emp_no int NOT NULL,
+	emp_title_id VARCHAR(40) NOT NULL,
+	birth_date date,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	sex varchar(1) NOT NULL,
+	hire_date date NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+
+CREATE TABLE salaries(
+	emp_no int,
+	salary int,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE departments(
+	dept_no VARCHAR(4) NOT NULL ,
+	dept_name VARCHAR (50),
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no int NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no int NOT NULL,
+	dept_no VARCHAR(4) NOT NULL ,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+
+
+
